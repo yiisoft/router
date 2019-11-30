ddd6e7328d265185ed733db482f101cbaf2934a6
Alexander Makarov <sam@rmcreative.ru>
2019-12-01 00:36:41 +0300

    Add tests for Group

--- tests/GroupTest.php
+++ tests/GroupTest.php
@@ -4,21 +4,44 @@
 namespace Yiisoft\Router\Tests;
 
 use PHPUnit\Framework\TestCase;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
 use Yiisoft\Router\Group;
+use Yiisoft\Router\GroupMiddlewareInterface;
 use Yiisoft\Router\Route;
 
 class GroupTest extends TestCase
 {
+    public function testGetPrefix(): void
+    {
+        $group = new Group('/api');
+        $this->assertSame('/api', $group->getPrefix());
+    }
+
     public function testGetRoutes(): void
     {
-        $apiRoutes = (new Group('/api'))
-            ->addRoute(Route::get('/posts'))
-            ->addRoute(Route::get('/comments'));
+        $routes = [
+            Route::get('/posts'),
+            Route::get('/comments')
+        ];
 
+        $group = new Group('/api');
 
-        foreach ($apiRoutes->getRoutes() as $route) {
-            /* @var Route $route */
-            $this->assertStringStartsWith('/api', $route->getPattern());
+        foreach ($routes as $route) {
+            $group = $group->addRoute($route);
         }
+
+        $this->assertCount(2, $group->getRoutes());
+        $this->assertSame($routes, $group->getRoutes());
+    }
+
+    public function testGetMiddleware(): void
+    {
+        $middleware = $this->getMockBuilder(GroupMiddlewareInterface::class)->getMock();
+
+        $group = new Group('/api', $middleware);
+        $this->assertSame($middleware, $group->getMiddleware());
     }
 }
