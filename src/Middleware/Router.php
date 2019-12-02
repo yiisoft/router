cbba288215ad1d8f2608feed1698234e1cc11867
Alexander Makarov <sam@rmcreative.ru>
2019-12-02 21:27:24 +0300

    Fix #1: Add groups support

--- src/Middleware/Router.php
+++ src/Middleware/Router.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Yiisoft\Router\Middleware;
 
 use Psr\Http\Message\ResponseFactoryInterface;
---
+++ tests/GroupTest.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace Yiisoft\Router\Tests;
+
+use PHPUnit\Framework\TestCase;
+use Yiisoft\Router\Group;
+use Yiisoft\Router\GroupMiddlewareInterface;
+use Yiisoft\Router\Route;
+
+class GroupTest extends TestCase
+{
+    public function testGetPrefix(): void
+    {
+        $group = new Group('/api');
+        $this->assertSame('/api', $group->getPrefix());
+    }
+
+    public function testGetRoutes(): void
+    {
+        $routes = [
+            Route::get('/posts'),
+            Route::get('/comments')
+        ];
+
+        $group = new Group('/api');
+
+        foreach ($routes as $route) {
+            $group = $group->addRoute($route);
+        }
+
+        $this->assertCount(2, $group->getRoutes());
+        $this->assertSame($routes, $group->getRoutes());
+    }
+
+    public function testGetMiddleware(): void
+    {
+        $middleware = $this->getMockBuilder(GroupMiddlewareInterface::class)->getMock();
+
+        $group = new Group('/api', $middleware);
+        $this->assertSame($middleware, $group->getMiddleware());
+    }
+}
