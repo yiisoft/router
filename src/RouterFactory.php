7706a72831b3c1c12be97f9161095574ab793c40
Alexander Makarov <sam@rmcreative.ru>
2019-12-05 00:12:06 +0300

    Adjust factory

--- src/RouterFactory.php
+++ src/RouterFactory.php
@@ -21,7 +21,13 @@ final class RouterFactory
         /* @var $router RouterInterface */
         $router = $factory($container);
         foreach ($this->routes as $route) {
-            $router->addRoute($route);
+            if ($route instanceof Route) {
+                $router->addRoute($route);
+            } elseif (is_array($route) && count($route) === 2 && is_string($route[0]) && is_callable($route[1])) {
+                $router->addGroup($route[0], $route[1]);
+            } else {
+                throw new \InvalidArgumentException('Routes should be eithe instances of Route or group arrays');
+            }
         }
         return $router;
     }
