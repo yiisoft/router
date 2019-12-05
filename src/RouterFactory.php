44c5beb7a7ae583e5374f51f6937ffa552a5ada6
Alexander Makarov <sam@rmcreative.ru>
2019-12-05 14:31:15 +0300

    Update src/RouterFactory.php
    Co-Authored-By: Rustam Mamadaminov <rmamdaminov@gmail.com>

--- src/RouterFactory.php
+++ src/RouterFactory.php
@@ -23,7 +23,7 @@ final class RouterFactory
         foreach ($this->routes as $route) {
             if ($route instanceof Route) {
                 $router->addRoute($route);
-            } elseif (is_array($route) && count($route) === 2 && is_string($route[0]) && is_callable($route[1])) {
+            } elseif (\is_array($route) && \count($route) === 2 && \is_string($route[0]) && \is_callable($route[1])) {
                 $router->addGroup($route[0], $route[1]);
             } else {
                 throw new \InvalidArgumentException('Routes should be eithe instances of Route or group arrays');
