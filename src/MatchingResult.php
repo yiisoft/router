3941ccc4ef16f3ad0518bc5e3bb5f77988f83699
Alexander Makarov <sam@rmcreative.ru>
2020-01-04 20:32:27 +0300

    Add missing imports

--- src/MatchingResult.php
+++ src/MatchingResult.php
@@ -6,6 +6,7 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\MiddlewareInterface;
 use Psr\Http\Server\RequestHandlerInterface;
+use Yiisoft\Http\Method;
 
 final class MatchingResult implements MiddlewareInterface
 {
