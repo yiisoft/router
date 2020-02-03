898e54da7595a97dd008c3fefd594a534cf541ad
yiiliveext <yiiliveext@gmail.com>
2020-02-03 13:41:22 +0200

    Update RouterCollectorInterface, add/fix phpdoc

--- src/RouteCollectorInterface.php
+++ src/RouteCollectorInterface.php
@@ -15,14 +15,30 @@ interface RouteCollectorInterface
      * Add a prefix for a group of routes
      *
      * ```php
-     * $router->addGroup('/api', function (RouteCollectorInterface $router) {
-     *     $router->addRoute(...);
-     *     $router->addGroup(...);
-     * })->addMiddleware($myMiddleware);
+     * $router->addGroup('/api', function (Group $group) {
+     *     $group->addRoute(Route::get('/users', function () {}));
+     *     $group->addGroup(Route::get('/contacts', function () {}));
+     *     $group->addMiddleware($myMiddleware);
+     * });
      * ```
      *
      * @param string $prefix
      * @param callable $callback
      */
     public function addGroup(string $prefix, callable $callback): void;
+
+    /**
+     * Add a group of routes
+     *
+     * ```php
+     * $group = Group::create('/api', [
+     *     Route::get('/users', function () {}),
+     *     Route::get('/contacts', function () {}),
+     * ])->addMiddleware($myMiddleware);
+     * $router->addGroupInstance($group);
+     * ```
+     *
+     * @param Group $group
+     */
+    public function addGroupInstance(Group $group): void;
 }
