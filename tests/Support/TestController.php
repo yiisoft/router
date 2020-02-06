8768dd9b5a4d4da414b243086410837f15d341bf
Alexander Makarov <sam@rmcreative.ru>
2020-02-06 17:23:40 +0300

    Minor cleanup in tests support

--- tests/Support/TestController.php
+++ tests/Support/TestController.php
@@ -5,14 +5,8 @@ namespace Yiisoft\Router\Tests\Support;
 use Nyholm\Psr7\Response;
 use Psr\Http\Message\ResponseInterface;
 
-/**
- * ETestController
- */
 class TestController
 {
-    /**
-     * @return string
-     */
     public function index(): ResponseInterface
     {
         return new Response();
