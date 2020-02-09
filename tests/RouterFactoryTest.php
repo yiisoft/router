bd74a2ba99340d9854ceaf3f80c8434ddd73e6c8
Dmitriy Derepko <xepozz@list.ru>
2020-02-09 16:49:17 +0300

    Improved test: 1. Removed functional test, because all checks are exists in GroupTest and RouteTest classes. 2. Covered that RouterFactory handle or not the `withContainer` method.

--- tests/RouterFactoryTest.php
+++ tests/RouterFactoryTest.php
@@ -1,14 +1,9 @@
 <?php
 
-
 namespace Yiisoft\Router\Tests;
 
 use PHPUnit\Framework\TestCase;
 use Psr\Container\ContainerInterface;
-use Psr\Http\Message\ServerRequestInterface;
-use Yiisoft\Router\Group;
-use Yiisoft\Router\MatchingResult;
-use Yiisoft\Router\Route;
 use Yiisoft\Router\RouterFactory;
 use Yiisoft\Router\RouterInterface;
 use Yiisoft\Router\Tests\Support\Container;
@@ -17,109 +12,38 @@ final class RouterFactoryTest extends TestCase
 {
     public function testContainerInjected(): void
     {
-        $container = $this->getContainer();
+        $router = $this->createMock(RouterInterface::class);
+        $router
+            ->expects($this->once())
+            ->method('withContainer')
+            ->willReturn($this->createMock(RouterInterface::class));
+        $router
+            ->expects($this->once())
+            ->method('hasContainer');
 
-        $routes = [
-            Route::get('/info')->name('api-info'),
-            Group::create(
-                '/v2',
-                [
-                    Route::get('/user')->name('api-v2-user/index'),
-                    Route::get('/user/{id}')->name('api-v2-user/view'),
-                    Group::create(
-                        '/news',
-                        [
-                            Route::get('/post')->name('api-v2-news-post/index'),
-                            Route::get('/post/{id}')->name('api-v2-news-post/view'),
-                        ]
-                    ),
-                    Group::create(
-                        '/blog',
-                        [
-                            Route::get('/post')->name('api-v2-blog-post/index'),
-                            Route::get('/post/{id}')->name('api-v2-blog-post/view'),
-                        ]
-                    ),
-                    Route::get('/note')->name('api-v2-note/index'),
-                    Route::get('/note/{id}')->name('api-v2-note/view'),
-                ]
-            ),
-            Group::create(
-                '/v2',
-                [
-                    Route::get('/user')->name('api-v2-user/index'),
-                    Route::get('/user/{id}')->name('api-v2-user/view'),
-                    Group::create(
-                        '/news',
-                        [
-                            Route::get('/post')->name('api-v2-news-post/index'),
-                            Route::get('/post/{id}')->name('api-v2-news-post/view'),
-                        ]
-                    ),
-                    Group::create(
-                        '/blog',
-                        [
-                            Route::get('/post')->name('api-v2-blog-post/index'),
-                            Route::get('/post/{id}')->name('api-v2-blog-post/view'),
-                        ]
-                    ),
-                    Route::get('/note')->name('api-v2-note/index'),
-                    Route::get('/note/{id}')->name('api-v2-note/view'),
-                ]
-            )
-        ];
-
-        $factory = new RouterFactory($this->getEngineFactory(), $routes);
-        $router = $factory($container);
-        $items = $router->getItems();
-
-        $this->assertAllRoutesAndGroupsHaveContainer($items);
+        $factory = new RouterFactory(fn() => $router);
+        $newRouter = $factory($this->getContainer());
+        $this->assertNotSame($router, $newRouter);
     }
 
-    private function assertAllRoutesAndGroupsHaveContainer(array $items): void
+    public function testContainerNotInjected(): void
     {
-        $func = function ($item) use (&$func) {
-            $this->assertTrue($item->hasContainer());
-            if ($item instanceof Group) {
-                $items = $item->getItems();
-                array_walk($items, $func);
-            }
-        };
-        array_walk($items, $func);
+        $router = $this->createMock(RouterInterface::class);
+        $router
+            ->expects($this->never())
+            ->method('withContainer');
+        $router
+            ->expects($this->once())
+            ->method('hasContainer')
+            ->willReturn(true);
+
+        $factory = new RouterFactory(fn() => $router);
+        $newRouter = $factory($this->getContainer());
+        $this->assertSame($router, $newRouter);
     }
 
     private function getContainer(array $instances = []): ContainerInterface
     {
         return new Container($instances);
-    }
-
-    private function getEngineFactory(): callable
-    {
-        return new class() {
-            public function __invoke(): RouterInterface
-            {
-                return new class() extends Group implements RouterInterface {
-                    public function match(ServerRequestInterface $request): MatchingResult
-                    {
-                    }
-
-                    public function generateAbsolute(string $name, array $parameters = [], string $scheme = null, string $host = null): string
-                    {
-                    }
-
-                    public function generate(string $name, array $parameters = []): string
-                    {
-                    }
-
-                    public function getUriPrefix(): string
-                    {
-                    }
-
-                    public function setUriPrefix(string $name): void
-                    {
-                    }
-                };
-            }
-        };
     }
 }
