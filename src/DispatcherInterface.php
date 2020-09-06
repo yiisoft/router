adbf7aa7825ec687553a5872d829dfef3a648415
Alexander Makarov <sam@rmcreative.ru>
2020-09-06 15:18:59 +0300

    Add description to DispatcherInterface

--- src/DispatcherInterface.php
+++ src/DispatcherInterface.php
@@ -8,6 +8,11 @@ use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Psr\Http\Server\RequestHandlerInterface;
 
+/**
+ * Dispatcher takes care of executing middleware stack given request object.
+ * Default implementation is {@see Dispatcher}. You may want to make your own
+ * implementation to support extra functionality in route handlers.
+ */
 interface DispatcherInterface
 {
     public function dispatch(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface;
