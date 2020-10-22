48db6c4696d8b691efa1c9bbec6ae3269d052d70
strorch <smy980807@ukr.net>
2020-10-22 13:15:44 +0300

    removed useless middleware configs after #75

--- config/common.php
+++ config/common.php
@@ -1,14 +1,8 @@
 <?php
 
-use Yiisoft\Router\MiddlewareStack;
-use Yiisoft\Router\MiddlewareStackInterface;
 use Yiisoft\Router\Group;
-use Yiisoft\Router\MiddlewareFactoryInterface;
 use Yiisoft\Router\RouteCollectorInterface;
-use Yiisoft\Router\MiddlewareFactory;
 
 return [
-    MiddlewareStackInterface::class => Middlewarestack::class,
-    MiddlewareFactoryInterface::class => MiddlewareFactory::class,
     RouteCollectorInterface::class => Group::create(),
 ];
