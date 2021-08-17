2b384333546f3b26f02076ad21a60e631e744c67
Rustam <rmamdaminov@gmail.com>
2021-08-17 14:54:39 +0500

    Adjustments

--- config/common.php
+++ config/common.php
@@ -4,10 +4,9 @@ declare(strict_types=1);
 
 use Yiisoft\Router\RouteCollector;
 use Yiisoft\Router\RouteCollectorInterface;
-use Yiisoft\Router\Router;
-use Yiisoft\Router\RouterInterface;
+use Yiisoft\Router\CurrentRoute;
 
 return [
     RouteCollectorInterface::class => RouteCollector::class,
-    RouterInterface::class => Router::class,
+    CurrentRoute::class => CurrentRoute::class,
 ];
