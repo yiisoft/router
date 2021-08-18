694aa09cbd0085ac10f4a3eed0eaf7fb6769ee5d
Rustam Mamadaminov <rmamdaminov@gmail.com>
2021-08-18 13:54:03 +0500

    #56 Refactor matcher so current URL is stored/accessed seprately (#117)

--- src/UrlMatcherInterface.php
+++ src/UrlMatcherInterface.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Yiisoft\Router;
 
 use Psr\Http\Message\ServerRequestInterface;
-use Psr\Http\Message\UriInterface;
 
 /**
  * UrlMatcherInterface allows finding a matching route given a PSR-8 server request. It is preferred to type-hint
@@ -14,18 +13,4 @@ use Psr\Http\Message\UriInterface;
 interface UrlMatcherInterface
 {
     public function match(ServerRequestInterface $request): MatchingResult;
-
-    /**
-     * Returns the current Route object
-     *
-     * @return RouteParametersInterface|null current route
-     */
-    public function getCurrentRoute(): ?RouteParametersInterface;
-
-    /**
-     * Returns current URI
-     *
-     * @return UriInterface|null current URI
-     */
-    public function getCurrentUri(): ?UriInterface;
 }
---
+++ tests/CurrentRouteTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Router\Tests;
+
+use Nyholm\Psr7\Uri;
+use Yiisoft\Router\Route;
+use Yiisoft\Router\CurrentRoute;
+use PHPUnit\Framework\TestCase;
+
+class CurrentRouteTest extends TestCase
+{
+    public function testGetCurrentRoute()
+    {
+        $route = Route::get('')->name('test');
+        $currentRoute = new CurrentRoute();
+        $currentRoute->setRoute($route);
+
+        $this->assertSame($route, $currentRoute->getRoute());
+    }
+
+    public function testGetCurrentUri()
+    {
+        $uri = new Uri('/test');
+        $currentRoute = new CurrentRoute();
+        $currentRoute->setUri($uri);
+
+        $this->assertSame($uri, $currentRoute->getUri());
+    }
+}
