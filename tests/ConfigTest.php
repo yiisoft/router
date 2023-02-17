fe1df9a7c8eb4e1c7aa010cb7cc437b69c227154
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-02-17 17:39:36 +0300

    Adapt configuration group names to Yii conventions (#187)

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -45,14 +45,14 @@ final class ConfigTest extends TestCase
     {
         return new Container(
             ContainerConfig::create()->withDefinitions(
-                $this->getCommonDefinitions()
+                $this->getContainerDefinitions()
             )
         );
     }
 
-    private function getCommonDefinitions(): array
+    private function getContainerDefinitions(): array
     {
         $params = [];
-        return require dirname(__DIR__) . '/config/common.php';
+        return require dirname(__DIR__) . '/config/di.php';
     }
 }
