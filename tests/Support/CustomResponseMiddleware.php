f3f2f85af0d1e3a7c31a9464defb6d35def0fa56
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-03-28 13:26:53 +0300

    Add route middleware tests

---
+++ tests/Support/CustomResponseMiddleware.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yiisoft\Router\Tests\Support;
+
+use Nyholm\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\MiddlewareInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+
+final class CustomResponseMiddleware implements MiddlewareInterface
+{
+    public function __construct(
+        private int $code,
+    ) {
+    }
+
+    public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
+    {
+        return new Response($this->code);
+    }
+}
