0892cbebfdfbe73ae9d2a64a133c96daf1552c7a
Sergei Predvoditelev <sergei@predvoditelev.ru>
2023-11-09 14:02:58 +0300

    Fix debug classes and add more tests (#224)

--- src/Debug/RouterCollector.php
+++ src/Debug/RouterCollector.php
@@ -12,9 +12,6 @@ use Yiisoft\Router\RouteCollectionInterface;
 use Yiisoft\Yii\Debug\Collector\CollectorTrait;
 use Yiisoft\Yii\Debug\Collector\SummaryCollectorInterface;
 
-/**
- * @codeCoverageIgnore
- */
 final class RouterCollector implements SummaryCollectorInterface
 {
     use CollectorTrait;
@@ -134,15 +131,10 @@ final class RouterCollector implements SummaryCollectorInterface
         if ($route === null) {
             return [[], null];
         }
-        $reflection = new ReflectionObject($route);
 
-        $reflectionProperty = $reflection->getProperty('middlewareDefinitions');
-        $reflectionProperty->setAccessible(true);
-        /**
-         * @var array[]|callable[]|string[] $middlewareDefinitions
-         */
-        $middlewareDefinitions = $reflectionProperty->getValue($route);
-        $action = array_pop($middlewareDefinitions);
-        return [$middlewareDefinitions, $action];
+        $middlewares = $route->getData('enabledMiddlewares');
+        $action = array_pop($middlewares);
+
+        return [$middlewares, $action];
     }
 }
