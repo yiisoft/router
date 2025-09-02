89061994ccccc842b3f1e14387eccd8d32ff9530
Oleg Baturin <baturin@gmail.com>
2025-09-02 18:40:37 +0700

    add tests for handlers

--- src/MethodNotAllowedHandler.php
+++ src/MethodNotAllowedHandler.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Yiisoft\Router;
 
+use InvalidArgumentException;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
@@ -17,8 +18,15 @@ class MethodNotAllowedHandler implements MethodFailureHandlerInterface
     public function __construct(private readonly ResponseFactoryInterface $responseFactory) {
     }
 
+    /**
+     * @throws InvalidArgumentException when methods are empty or not set
+     */
     public function handle(ServerRequestInterface $request): ResponseInterface
     {
+        if (empty($this->methods)) {
+            throw new InvalidArgumentException("Allowed methods can't be empty array.");
+        }
+
         return $this->responseFactory
                 ->createResponse(Status::METHOD_NOT_ALLOWED)
                 ->withHeader(Header::ALLOW, implode(', ', $this->methods));
